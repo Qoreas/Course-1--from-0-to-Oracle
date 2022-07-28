87851af7cf484808ceb0eb956e9061e56ca17695
Aykov <buyfrtscbhbivpz@gmail.com>
2022-07-28 13:48:59 +0300

    rebuild

--- Course 1 - from 0 to Oracle/src/HomeWork/Lessons_10_per_19/L_13/Months.java
+++ Course 1 - from 0 to Oracle/src/HomeWork/Lessons_10_per_19/L_13/Months.java
@@ -1,34 +1,34 @@
 package HomeWork.Lessons_10_per_19.L_13;
 
 public class Months {
-    static void showMonthDays(int number) {
+    static void showMonthDays(int number) { // year 2015
         switch (number) {
             default:
                 System.out.println("¬веден неверный пор€дковый номер мес€ца"); break;
             case 1:
-                System.out.println("январь имеет 25 дней"); break;
+                System.out.println("январь имеет 31 дней"); break;
             case 2:
-                System.out.println("‘евраль имеет 22 дн€"); break;
+                System.out.println("‘евраль имеет 28 дн€"); break;
             case 3:
-                System.out.println("ћарт имеет 29 дней"); break;
+                System.out.println("ћарт имеет 31 дней"); break;
             case 4:
-                System.out.println("јпрель имеет 26 дней"); break;
+                System.out.println("јпрель имеет 30 дней"); break;
             case 5:
                 System.out.println("ћай имеет 31 день"); break;
             case 6:
-                System.out.println("»юнь имеет 28 дней"); break;
+                System.out.println("»юнь имеет 30 дней"); break;
             case 7:
-                System.out.println("»юнь имеет 26 дней"); break;
+                System.out.println("»юнь имеет 31 дней"); break;
             case 8:
                 System.out.println("јвгуст имеет 30 дней"); break;
             case 9:
-                System.out.println("—ент€брь имеет 27 дней"); break;
+                System.out.println("—ент€брь имеет 30 дней"); break;
             case 10:
-                System.out.println("ќкт€брь имеет 25 дней"); break;
+                System.out.println("ќкт€брь имеет 31 дней"); break;
             case 11:
-                System.out.println("Ќо€брь имеет 29 дней"); break;
+                System.out.println("Ќо€брь имеет 30 дней"); break;
             case 12:
-                System.out.println("ƒекабрь имеет 27 дней"); break;
+                System.out.println("ƒекабрь имеет 31 дней"); break;
         }
     }
 
