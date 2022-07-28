027db536474d14a7935f33c31d9d549ff2b504ab
Aykov <buyfrtscbhbivpz@gmail.com>
2022-07-28 16:59:24 +0300

    add check lang

---
+++ Course 1 - from 0 to Oracle/src/checkLocaleLanguage.java
@@ -0,0 +1,8 @@
+import java.util.Locale;
+
+public class checkLocaleLanguage {
+    public static void main(String[] args) {
+        Locale current = Locale.getDefault();
+        System.out.println(current);
+    }
+}
