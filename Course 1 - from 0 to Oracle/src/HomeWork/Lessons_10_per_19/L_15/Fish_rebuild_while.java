58d0468e1506694d5606ef9ce62e33dd41e4d638
iiwfni <buyfrtscbhbivpz@gmail.com>
2022-08-02 11:27:29 +0300

    homework

---
+++ Course 1 - from 0 to Oracle/src/HomeWork/Lessons_10_per_19/L_15/Fish_rebuild_while.java
@@ -0,0 +1,42 @@
+package HomeWork.Lessons_10_per_19.L_15;
+
+public class Fish_rebuild_while {
+    static void showTime() {
+        int hour = 0;
+        OUTER:
+        while (hour < 6) {
+            System.out.println("Начало цикла");
+
+            int minutes = -1;
+            MIDDLE:
+            do {
+                minutes++;
+
+                if (hour > 1 && minutes % 10 == 0) {
+                    System.out.println("Конец метода");
+                    break OUTER;
+                }
+
+                int seconds = 0;
+                INNER:
+                while (seconds < 60) {
+
+                    if (seconds * hour > minutes) {
+                        System.out.println("Следующая минута");
+                        continue MIDDLE;
+                    }
+
+                    System.out.println(hour + ":" + minutes + ":" + seconds);
+                    seconds++;
+                }
+
+            } while (minutes < 59);
+
+            hour++;
+        }
+    }
+
+    public static void main(String[] args) {
+        showTime();
+    }
+}
