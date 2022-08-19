fd09ed2ca672706e79aab542c9cf0ac71d568b0c
Qoreas <buyfrtscbhbivpz@gmail.com>
2022-08-19 22:53:52 +0300

    classwork

---
+++ Course 1 - from 0 to Oracle/src/ClassWork/Lessons_20_per_29/L_22_OOP/Encapsulation/Human02.java
@@ -0,0 +1,55 @@
+package ClassWork.Lessons_20_per_29.L_22_OOP.Encapsulation;
+
+public class Human02 {
+
+    private boolean cleaver;
+    public boolean isCleaver() {
+        return cleaver;
+    }
+    public void setCleaver(boolean b) {
+        cleaver = b;
+    }
+    final String sex;
+    private StringBuilder name;
+    // encapsulation
+    public StringBuilder getName() {
+        StringBuilder sb = new StringBuilder(name);
+        return sb;
+    }
+    public void setName(StringBuilder s) {
+        name = s;
+    }
+
+    private int age;
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int i) {
+        if (i > 0) {
+            age = i;
+        }
+    }
+
+    private int weight;
+    public int getWeight() {
+        return weight;
+    }
+    public void setWeight(int weight) {
+        if (weight > 0) {
+            this.weight = weight;
+        }
+    }
+    public Human02(String sex) {
+        this.sex = sex;
+    }
+}
+
+class test_1 {
+    public static void main(String[] args) {
+        Human02 human = new Human02("male");
+        human.setName(new StringBuilder("Kolya"));
+        human.getName().append("!!!");
+        System.out.println(human.getName());
+
+    }
+}
---
+++ Course 1 - from 0 to Oracle/src/ClassWork/Lessons_20_per_29/L_22_OOP/Inheritance/Extends.java
@@ -0,0 +1,58 @@
+package ClassWork.Lessons_20_per_29.L_22_OOP.Inheritance;
+
+public class Extends {
+    void plusSalary(Employee employee) {
+        employee.salary += 100;
+    }
+
+    public static void main(String[] args) {
+        Doc doc = new Doc();
+        doc.name = "Ivan";
+        doc.age = 25;
+        doc.experience = 10;
+        doc.eat();
+        doc.heal();
+
+    }
+}
+
+class Employee {
+    double salary = 100;
+    String name;
+    int age;
+    int experience;
+
+    void eat() {System.out.println("Eating");}
+    void sleep() {System.out.println("Sleeping");}
+}
+
+
+class Doc extends Employee{
+    void heal() {System.out.println("Healing");}
+}
+
+class medBrother extends Doc {
+    String scalp;
+    void operation() {}
+}
+
+class dentist extends Doc {
+
+}
+
+
+
+
+
+
+class Teacher extends Employee{
+    int countOfStudents;
+
+    void teach() {System.out.println("Teaching");}
+}
+
+class Driver extends Employee{
+    String carName;
+
+    void drive() {System.out.println("Driving");}
+}
