e436226a11b2d371b82fef254dba63377a07930e
Qoreas <buyfrtscbhbivpz@gmail.com>
2022-08-31 00:33:15 +0300

    hotFix

--- Course 1 - from 0 to Oracle/src/Fun/OGE/ex01.java
+++ Course 1 - from 0 to Oracle/src/Fun/OGE/ex01.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class ex01 {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] array = new int[1000];
+        int[] array = new int[1001];
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(30_000);
         }
