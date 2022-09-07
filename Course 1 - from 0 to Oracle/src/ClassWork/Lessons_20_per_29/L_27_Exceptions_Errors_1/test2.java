db463f9834b5ebb0e6283a3f92d78a90fe98c1a9
Qoreas <buyfrtscbhbivpz@gmail.com>
2022-09-07 20:20:06 +0300

    classwork

--- Course 1 - from 0 to Oracle/src/ClassWork/Lessons_20_per_29/L_27_Exceptions_Errors_1/test2.java
+++ Course 1 - from 0 to Oracle/src/ClassWork/Lessons_20_per_29/L_27_Exceptions_Errors_1/test2.java
@@ -1,12 +1,20 @@
 package ClassWork.Lessons_20_per_29.L_27_Exceptions_Errors_1;
 
 public class test2 {
-    public static void main(String[] args) {
-        int[] array = {1, 2, 3};
-        String s = null;
 
+    void abc() throws NullPointerException {
+        String s = null;
         System.out.println(s.length());
-        System.out.println(array[5]);
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int[] array = {1, 2, 3};
+        try {
+            System.out.println(array[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("the exception was caught - " + e);
+        }
         System.out.println("hello");
     }
 }
--- Course 1 - from 0 to Oracle/src/ClassWork/Lessons_20_per_29/L_27_Exceptions_Errors_1/try_catch_finally.java
+++ Course 1 - from 0 to Oracle/src/ClassWork/Lessons_20_per_29/L_27_Exceptions_Errors_1/try_catch_finally.java
@@ -3,23 +3,35 @@ package ClassWork.Lessons_20_per_29.L_27_Exceptions_Errors_1;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class try_catch_finally {
+
+    void abc() throws FileNotFoundException {
+        File file = new File("C:\\Users\\Qreas\\IdeaProjects\\Study\\Course 1 - from 0 to Oracle\\src\\ClassWork\\Lessons_20_per_29\\L_27_Exceptions_Errors_1\\java_io\\testing_io.txt");
+        FileInputStream fis = new FileInputStream(file);
+    }
+
+    void def() throws FileNotFoundException {
+        abc();
+    }
+
     public static void main(String[] args) {
-        File file = new File("C:\\Users\\Qoreas\\IdeaProjects\\Study\\Course 1 - from 0 to Oracle\\src\\ClassWork\\Lessons_20_per_29\\L_27_Exceptions_Errors_1\\java_io\\testing_io.txt");
+        // exception to search
+//         catch (IOException e) {
+//            System.out.println("Can not be read");
+//        } finally {
+//            System.out.println("This is finally block");
+//        }
+        try_catch_finally t = new try_catch_finally();
 
         try {
-            FileInputStream fis = new FileInputStream(file);
-            System.out.println("Have a good day");
+            t.def();
+        } catch (FileNotFoundException e) {
+            IOException f = e;
+            System.out.println("Exception was caught");
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
         }
-        // exception to search
-        catch (FileNotFoundException e) {
-            System.out.println("We caught the exception at - " + e);
-        } finally {
-            System.out.println("This is finally block");
-        }
-
-        System.out.println("another code");
-
     }
 }
