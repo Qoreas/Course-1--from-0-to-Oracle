e77d74f198a5b8e8b0fc26218bd7b0e5738480ef
Qoreas <buyfrtscbhbivpz@gmail.com>
2022-10-06 21:49:39 +0300

    classwork

---
+++ Course 1 - from 0 to Oracle/src/ClassWork/Lessons_29_per_End/L33_WorkWithCmd/CmdTest.java
@@ -0,0 +1,7 @@
+package ClassWork.Lessons_29_per_End.L33_WorkWithCmd;
+
+public class CmdTest {
+    public static void main(String[] args) {
+        System.out.println("Preparing to exam");
+    }
+}
